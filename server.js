b373afd308954722d4c29d118c0a223bf197b6cf
Robert Quartey <37972474+robertquartey7@users.noreply.github.com>
2023-02-28 16:06:20 -0500

    .

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import authRoutes from "./controllers/auth/index.js";
 import morgan from "morgan";
+import passport from "passport";
+import setupJWTStrategy from "./controllers/auth/index.js";
 // creating a server
 export default async function createServer() {
   const app = express();
 
   app.use(express.json());
+  setupJWTStrategy(passport);
   app.use("auth", morgan("tiny"), authRoutes);
 
   return app;
